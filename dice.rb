efc7a04b809c847c084e457a0e2e3c22a9edda13
Federico Hernandez Nater <166563763+fhernandeznater@users.noreply.github.com>
2025-01-14 23:41:29 +0000

    Added more dice

--- dice.rb
+++ dice.rb
@@ -36,3 +36,22 @@ get("/dice/2/10") do
   "<h1>2d6</h1>
   <p>#{outcome}</p>"
 end
+
+get("/dice/1/20") do
+  die = rand(1..20)
+
+  outcome = "You rolled a #{die}."
+
+  "<h1>1d20</h1>
+  <p>#{outcome}</p>"
+
+get("/dice/5/4") do
+  first_die = rand(1..4)
+  second_die = rand(1..4)
+  third_die = rand(1..4)
+  fourth_die = rand(1..4)
+  fifth_die = rand(1..4)
+
+  sum = first_die + second_die + third_die + fourth_die + fifth_die
+
+  outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, a #{fourth_die}, and a #{fifth_die}. Your total is #{sum}."
