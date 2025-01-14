a56a0c41509604566884214bae7c2e8353b65454
Federico Hernandez Nater <166563763+fhernandeznater@users.noreply.github.com>
2025-01-14 23:59:04 +0000

    closing tags

--- dice.rb
+++ dice.rb
@@ -6,11 +6,11 @@ require "sinatra/reloader"
 get '/' do
   "<h1>Dice Roll</h1>
   <ul>
-    <li><a href="/dice/2/6">Roll two 6-sided dice</li>
-    <li><a href="/dice/2/10">Roll two 10-sided dice</li>
-    <li><a href="/dice/1/20">Roll one 20-sided dice</li>
-    <li><a href="/dice/5/4">Roll five 4-sided dice</li>
-    <li><a href="/dice/1/8">Roll one 8-sided dice</li>
+    <li><a href="/dice/2/6">Roll two 6-sided dice</a></li>
+    <li><a href="/dice/2/10">Roll two 10-sided dice</a></li>
+    <li><a href="/dice/1/20">Roll one 20-sided dice</a></li>
+    <li><a href="/dice/5/4">Roll five 4-sided dice</a></li>
+    <li><a href="/dice/1/8">Roll one 8-sided dice</a></li>
   </ul>"
 end
 
