d1939fc1d1b89fa62f5e0ae295941c3b94c38fb8
Federico Hernandez Nater <166563763+fhernandeznater@users.noreply.github.com>
2025-01-15 00:07:56 +0000

    Final commit, project done!

--- dice.rb
+++ dice.rb
@@ -2,14 +2,14 @@
 
 require "sinatra"
 require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
 
 # Need this configuration for better_errors
 use(BetterErrors::Middleware)
 BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
-require "better_errors"
-require "binding_of_caller"
 
 get ("/") do
   "<h1>Dice Roll</h1>
