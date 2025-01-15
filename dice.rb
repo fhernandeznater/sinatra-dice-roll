e2f8f95f50fc66e055dbac207aa7304ea1091d94
Federico Hernandez Nater <166563763+fhernandeznater@users.noreply.github.com>
2025-01-15 00:06:53 +0000

    Added comment

--- dice.rb
+++ dice.rb
@@ -3,6 +3,7 @@
 require "sinatra"
 require "sinatra/reloader"
 
+# Need this configuration for better_errors
 use(BetterErrors::Middleware)
 BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
